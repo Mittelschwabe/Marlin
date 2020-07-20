5e4204186ca52f976c81ee014a90bb42f4e66790
Tobias Stötter <tobias.stoetter@smart-access-solutions.com>
2020-07-20 09:05:59 +0200

    delete lines

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -18,8 +18,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  I added this line
- *  I will delete this line an the ine above with the next release
  */
 #pragma once
 
